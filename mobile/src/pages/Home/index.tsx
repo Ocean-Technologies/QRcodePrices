8448296472bd26fed4cbf8424745b60c44a4d69d
Nathan Vieira <nathanvieira@lett.digital>
2020-12-15 10:52:53 -0300

    QR code working fine

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,26 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+
 import Button from '../../components/Button'
+import {
+  Text,
+  View,
+  StyleSheet,
+} from 'react-native';
+// import Constants from 'expo-constants';
+import * as Permissions from 'expo-permissions';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 import { useAuth } from '../../hooks/auth'
 
-export default function App() {
-  const { signOut } = useAuth()
+const Home: React.FC = () => {
+  const { signOut, user } = useAuth()
+  const [hasCameraPermission,setHasCameraPermission] = useState<boolean | null>(null)
+  const [scanned,setScanned] = useState<boolean | null>(null)
+
+  useEffect(()=>{
+    const exec = async ()=> {
+      const {
+        status
+      } = await Permissions.askAsync(Permissions.CAMERA);
+      setHasCameraPermission(status === 'granted');
+    } 
+    exec();
+
+  },[])
+
+  const rendeVariable = useMemo(()=>{
+    if (hasCameraPermission === null) {
+      return <Text> Requesting
+      for camera permission </Text>;
+    }
+    if (hasCameraPermission === false) {
+      return <Text> No access to camera </Text>;
+    }
+  },[hasCameraPermission])
+
+  const handleBarCodeScanned = useCallback(({
+    type,
+    data
+  }) => {
+    setScanned(true)
+    //TODO enviar para o backend os dados de usuario e produto
+    alert(`Bar code with type ${user.id} and data ${data} has been scanned!`);
+  },[user])
+
+  const handleSignOut = useCallback(()=>{
+    signOut()
+  },
+    [signOut],
+  )
 
   return (
-    <View style={styles.container}>
-      <Text>Bem vindo, autenticacao deu certo !</Text>
-      <StatusBar style="auto" />
-      <Button onPress={signOut} > Deslogar </Button>
+    <>
+    {rendeVariable && !rendeVariable ? rendeVariable : 
+    
+    <View style = {
+        {
+          flex: 1,
+          flexDirection: 'column',
+          justifyContent: 'flex-end',
+        }
+      }
+    >
+      <BarCodeScanner onBarCodeScanned = {
+        scanned ? undefined : handleBarCodeScanned
+      }
+      style = {
+        StyleSheet.absoluteFillObject
+      }
+      />
+
+      <Button onPress={handleSignOut}> Deslogar</Button>
+
+      {scanned && ( 
+        <Button title = {
+            'Tap to Scan Again'
+          }
+          onPress = {
+            () => setScanned(false)
+          }
+        />
+      )} 
     </View>
+  }
+  </>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default Home
